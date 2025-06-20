16237d6bf4e5a3ba4c077cac7556a9faaaa8dff7
gcutrini <gabriel.cutrini@gmail.com>
2025-06-20 14:45:37 -0300

    Dynamically load canvas renderer to avoid SSR issues

--- examples/01-react/src/pages/canvas.tsx
+++ examples/01-react/src/pages/canvas.tsx
@@ -3,7 +3,13 @@ import { RekaProvider, observer } from '@rekajs/react';
 import * as t from '@rekajs/types';
 import * as React from 'react';
 
-import { CanvasRenderer } from '@/components/CanvasRenderer';
+import dynamic from 'next/dynamic';
+
+const CanvasRenderer = dynamic(
+  () =>
+    import('@/components/CanvasRenderer').then((mod) => mod.CanvasRenderer),
+  { ssr: false }
+);
 import { Editor } from '@/components/Editor';
 
 const reka = Reka.create();
